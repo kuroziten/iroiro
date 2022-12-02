8212271ee3bccdabea9f60bde97f4696f4e658c0
kuroziten <33644935+kuroziten@users.noreply.github.com>
2022-12-02 10:49:05 +0900

    Update zitenBot.py
    がんばった！

--- zitenBot.py
+++ zitenBot.py
@@ -6,6 +6,7 @@ import urllib.request
 import random
 import schedule
 import datetime
+import imghdr
 
 # 【そのURLがあるかどうかを調べる関数】
 def UrlChecker(urlname00):
@@ -14,7 +15,14 @@ def UrlChecker(urlname00):
         un=res.geturl()
         res.close()
         if un == urlname00:
-            return 1
+            imagedata = None
+            with urllib.request.urlopen(urlname00) as fp:
+                imagedata = fp.read()
+            imagetype = imghdr.what(None, h=imagedata)
+            if imagetype != None:
+                return 1
+            else:
+                return 0
         else:
             return 0
         pass
@@ -42,7 +50,7 @@ def login():
     info = {
         "language":"jp-JP",
         "icon":"bm",
-        "name":"macじてん",
+        "name":"ぼっと",
         "login":"login",
         "token":token
     }
@@ -64,7 +72,7 @@ login()
 def job():
     global session
     session.post("https://drrrkari.com/room/?ajax=1#", data={"message":datetime.datetime.now(),"valid":1})
-    time.sleep(5)
+    login()
 schedule.every().hour.at(":00").do(job)
 schedule.every().hour.at(":15").do(job)
 schedule.every().hour.at(":30").do(job)
@@ -91,7 +99,13 @@ while True:
                         id_bk = talk["id"]
                         if talk.find(attrs={'class':'body'}) != None:
                             t = talk.find(attrs={'class':'body'}).text
-                            if "/url" == t:
+                            if "/room " == t[0:6]:
+                                session.post("https://drrrkari.com/room/?ajax=1", data={
+                                    "room_name":t[6:len(t)]
+                                })
+                                session.post("https://drrrkari.com/room/?ajax=1#", data={"message":"部屋名を変更しました：" + t[6:len(t)],"valid":1})
+                                login()
+                            elif "/url" == t:
                                 session.post("https://drrrkari.com/room/?ajax=1#", data={"message":"直前の画像URL：" + path_bk,"valid":1})
                             elif "/remove http" == t[0:12]:
                                 path = t[ t.find(" http") + 1 : len(t)]
@@ -113,12 +127,12 @@ while True:
                                         "upimg":"アップロード" 
                                     }
                                     session.post("https://drrrkari.com/room/", data=info, files=file)
-                                    time.sleep(5)
+                                    login()
                             elif "/help" == t:
                                 print("ヘルプ送信します")
                                 res = session.post("https://drrrkari.com/room/?ajax=1#", data={"message":"[/help] [/img 画像URL] [/登録したい名前 登録したい画像URL]","valid":1})
                                 print("ヘルプ送信完了！")
-                                time.sleep(5)
+                                login()
                             else:
                                 t = talk.find(attrs={'class':'body'}).text
                                 if t[0:5] != "/img " and t[0:1] == "/" and " http" in t:
@@ -138,35 +152,39 @@ while True:
                                         print("エラー２します")
                                         session.post("https://drrrkari.com/room/?ajax=1#", data={"message" : "この画像は登録できません","valid":1})
                                         print("エラー２完了！")
-                                        time.sleep(5)
+                                        login()
                                 elif t[0:1] == "/" and not " " in t:
+                                    # print("画像送信開始：" + datetime.datetime.now())
+                                    print("送信処理：「" + t.strip() + "」")
                                     req = session.post(
                                         "https://naughty-davinci.153-122-198-71.plesk.page/d/commandGet",
                                         data={
                                             "command": t.strip()
                                         }
                                     )
-                                    print(req.text)
+                                    print("送信処理：「" + req.text + "」")
                                     if req.text != "":
-                                        print(req.text)
+                                        print("送信処理：「空チェッククリア」")
                                         if UrlChecker(req.text):
+                                            print("送信処理：「有効チェッククリア」")
                                             file = {
                                                 'img_path': urllib.request.urlopen(req.text)
                                             }
                                             res = session.post("https://drrrkari.com/room/", data={"upimg":"アップロード"}, files=file)
                                             path_bk = req.text
+                                            print("送信処理完了")
                                             login()
                                         else:
                                             print("エラー３します")
                                             session.post("https://drrrkari.com/room/?ajax=1#", data={"message" : "無効URL：" + req.text,"valid":1})
                                             print("エラー３完了！")
-                                            time.sleep(5)
+                                            login()
 
                                     else:
                                         print("エラー１します")
                                         session.post("https://drrrkari.com/room/?ajax=1#", data={"message" : "画像が登録されていません","valid":1})
                                         print("エラー１完了！")
-                                        time.sleep(5)
+                                        login()
                                 elif t[0:1] == "/" and t[len(t)-4:len(t)] == " all":
                                     command = t[ 0 : t.find(" ") ]
                                     req = session.post(
@@ -204,7 +222,7 @@ while True:
                             t = t[3:len(t)]
                             if "入室" in talk.text[len(talk.text) - 9:len(talk.text)]:
                                 session.post("https://drrrkari.com/room/?ajax=1#", data={"message":t + "さんようこそ！","valid":1})
-                                time.sleep(5)
+                                login()
                 talk_bk = talk
         schedule.run_pending()
         time.sleep(1)
