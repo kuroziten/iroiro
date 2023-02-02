bbf7d5af732bccac963e2c8856dbabc997dc40da
kuroziten <33644935+kuroziten@users.noreply.github.com>
2023-02-02 15:54:21 +0900

    Update drrDeYtbPlay.js

--- drrDeYtbPlay.js
+++ drrDeYtbPlay.js
@@ -22,15 +22,12 @@ const videoId = '2-i1gzWeeOI';
     innterDiv.style.minHeight = "100%";
     innterDiv.style.minWidth = "100%";
     
-    // 2. This code loads the IFrame Player API code asynchronously.
     var tag = document.createElement('script');
     
     tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
     var player;
     function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
@@ -42,14 +39,10 @@ const videoId = '2-i1gzWeeOI';
     });
     }
     
-    // 4. The API will call this function when the video player is ready.
     function onPlayerReady(event) {
     event.target.playVideo();
     }
     
-    // 5. The API calls this function when the player's state changes.
-    //    The function indicates that when playing a video (state=1),
-    //    the player should play for six seconds and then stop.
     function onPlayerStateChange(event) {
         console.log(event.data);
     }
