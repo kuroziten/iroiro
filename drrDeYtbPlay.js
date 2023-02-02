af9ece817bcbbede1d8fb64be72ebd9f7c8520c5
kuroziten <33644935+kuroziten@users.noreply.github.com>
2023-02-02 15:31:56 +0900

    デュラチャでユーチューブ再生する変なの！

---
+++ drrDeYtbPlay.js
@@ -0,0 +1,69 @@
+const videoId = '2-i1gzWeeOI';
+{
+    const div = document.createElement("div");
+    console.log(div);
+    document.body.append(div);
+    div.style.width = "100vw";
+    const messageBox = document.querySelector(".message_box");
+    const mbh = messageBox.clientHeight;
+    div.style.height = `calc(100vh - ${mbh}px)`;
+    div.style.backgroundColor = "red";
+    div.style.position = "fixed";
+    div.style.top = `${mbh}px`;
+    div.style.display = "flex";
+    div.style.justifyContent = "center";
+    div.style.alignItems = "center";
+    div.style.zIndex = "-1";
+    div.style.zIndex = "-1";
+
+    const innterDiv = document.createElement("div");
+    div.append(innterDiv);
+    innterDiv.id = "player";
+    innterDiv.style.minHeight = "100%";
+    innterDiv.style.minWidth = "100%";
+    
+    // 2. This code loads the IFrame Player API code asynchronously.
+    var tag = document.createElement('script');
+    
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
+    function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      videoId: videoId,
+      events: {
+        'onReady': onPlayerReady,
+        'onPlayerStateChange': onPlayerStateChange
+      }
+    });
+    }
+    
+    // 4. The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+    event.target.playVideo();
+    }
+    
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    function onPlayerStateChange(event) {
+        console.log(event.data);
+    }
+
+    let notPlayFlg = true;
+    setInterval(()=>{
+        if (player.getPlayerState() != 1 && !notPlayFlg) {
+            notPlayFlg = true;
+        };
+    },500);
+    document.body.addEventListener("click", function(){
+        if (notPlayFlg) {
+            notPlayFlg = false;
+            player.playVideo();
+        }
+    });
+};
