f4367d74f91a349a31b4983e106ffe98ba13e0cf
kuroziten <33644935+kuroziten@users.noreply.github.com>
2023-02-02 19:08:20 +0900

    Update drrDeYtbPlay.js

--- drrDeYtbPlay.js
+++ drrDeYtbPlay.js
@@ -1,4 +1,4 @@
-const videoId = 'I7J2MTwPXh8';
+const videoId = 'taixhqpjvK4';
 {
     const div = document.createElement("div");
     console.log(div);
@@ -24,7 +24,6 @@ const videoId = 'I7J2MTwPXh8';
     innterDiv.style.minHeight = "100%";
     innterDiv.style.minWidth = "100%";
     
-    // 2. This code loads the IFrame Player API code asynchronously.
     var tag = document.createElement('script');
     
     tag.src = "https://www.youtube.com/iframe_api";
@@ -33,9 +32,15 @@ const videoId = 'I7J2MTwPXh8';
     
     var player;
     function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      videoId: videoId
-    });
+        player = new YT.Player(
+            'player'
+            ,{
+                videoId: videoId
+            },{
+                'playsinline': 1
+            }
+            
+        );
     }
 
     let notPlayFlg = true;
@@ -55,9 +60,55 @@ const videoId = 'I7J2MTwPXh8';
     document.body.append(style);
     style.type = `text/css`;
     style.innerText = `
-body::-webkit-scrollbar{
-  display: none;
-}        
+        body::-webkit-scrollbar{
+            display: none;
+        }        
     `;
+
+    const menu = document.querySelector(".menu");
     
+    const newUpimg = document.createElement("li");
+    menu.prepend(newUpimg);
+    newUpimg.innerText = "画像送信";
+    newUpimg.style.textDecoration = "underline";
+    newUpimg.style.color = "pink";
+    const input_file = document.querySelector('#img_path');
+    newUpimg.addEventListener(
+        "click",
+        function () {
+            input_file.click();
+        }
+    );
+    input_file.addEventListener(
+        "change",
+        function () {
+            const fd = new FormData();
+        	fd.append('img_path', this.files[0]);
+            fd.append('upimg', 'アップロード');
+        	fetch( '/room/', {
+            	method: 'POST',
+            	body: fd
+        	})
+            
+        }
+    );
+
+    const upimg = document.getElementsByClassName("upimg")[0];
+    upimg.style.display = "none";
+    
+    const ytbZButton = document.createElement("li");
+    menu.prepend(ytbZButton);
+    ytbZButton.style.border = "1px solid blue";
+    ytbZButton.style.borderRadius = "100%";
+    ytbZButton.innerText = "　";
+    ytbZButton.addEventListener(
+        "click",
+        function () {
+            if (div.style.zIndex == "-1") {
+                div.style.zIndex = "0";
+            } else {
+                div.style.zIndex = "-1";
+            }
+        }
+    );    
 };
