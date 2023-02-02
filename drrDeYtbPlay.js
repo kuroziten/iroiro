b33963010b461778d5d53858f4ee724b7e2e56e1
kuroziten <33644935+kuroziten@users.noreply.github.com>
2023-02-02 17:17:46 +0900

    デュラチャの背景をYoutubeにするやつ！

--- drrDeYtbPlay.js
+++ drrDeYtbPlay.js
@@ -1,15 +1,17 @@
-const videoId = '2-i1gzWeeOI';
+const videoId = 'I7J2MTwPXh8';
 {
     const div = document.createElement("div");
     console.log(div);
     document.body.append(div);
     div.style.width = "100vw";
     const messageBox = document.querySelector(".message_box");
+    messageBox.style.backgroundColor = "rgba(0,0,0,0)";
+    messageBox.style.color = "white";
     const mbh = messageBox.clientHeight;
-    div.style.height = `calc(100vh - ${mbh}px)`;
+    div.style.height = `100vh`;
     div.style.backgroundColor = "red";
     div.style.position = "fixed";
-    div.style.top = `${mbh}px`;
+    div.style.top = `0`;
     div.style.display = "flex";
     div.style.justifyContent = "center";
     div.style.alignItems = "center";
@@ -22,6 +24,7 @@ const videoId = '2-i1gzWeeOI';
     innterDiv.style.minHeight = "100%";
     innterDiv.style.minWidth = "100%";
     
+    // 2. This code loads the IFrame Player API code asynchronously.
     var tag = document.createElement('script');
     
     tag.src = "https://www.youtube.com/iframe_api";
@@ -31,20 +34,8 @@ const videoId = '2-i1gzWeeOI';
     var player;
     function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-      videoId: videoId,
-      events: {
-        'onReady': onPlayerReady,
-        'onPlayerStateChange': onPlayerStateChange
-      }
+      videoId: videoId
     });
-    }
-    
-    function onPlayerReady(event) {
-    event.target.playVideo();
-    }
-    
-    function onPlayerStateChange(event) {
-        console.log(event.data);
     }
 
     let notPlayFlg = true;
@@ -59,4 +50,14 @@ const videoId = '2-i1gzWeeOI';
             player.playVideo();
         }
     });
+
+    const style = document.createElement("style");
+    document.body.append(style);
+    style.type = `text/css`;
+    style.innerText = `
+body::-webkit-scrollbar{
+  display: none;
+}        
+    `;
+    
 };
