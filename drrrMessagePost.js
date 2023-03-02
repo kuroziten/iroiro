f2ca2a6b8e59af6fa12524ce855bbabe4000a655
kuroziten <33644935+kuroziten@users.noreply.github.com>
2023-03-03 02:02:13 +0900

    jsでデュラチャのメッセージ送信するやつ！

---
+++ drrrMessagePost.js
@@ -0,0 +1,23 @@
+// 送信するメッセージ
+const message = `おっぱいぺろぺろにゃり`
+
+// メッセージ送信処理
+fetch(
+    "/room/?ajax=1#",
+    {
+        method: 'POST', 
+        body: setFormData({
+            message: message,
+            valid: 1
+        })
+    }
+);
+
+// FormData作成関数
+function setFormData(hash) {
+    const body = new FormData();
+    for (let key in hash) {
+        body.append(key, hash[key]);
+    }
+    return body;
+}
