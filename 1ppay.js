65942bc8f1359efe2dafda4f5af8fd85aee66b7b
kuroziten <33644935+kuroziten@users.noreply.github.com>
2023-03-07 20:06:29 +0900

    バブルソートみたいなやつ！

--- 1ppay.js
+++ 1ppay.js
@@ -20,3 +20,12 @@ for (bef of befList) {
     
 }
 aftList;
+
+/*
+先人が考えた短いやつ
+const befList = [5,4,2,3,1,6,8,7,5];
+for (i = 0; i < befList.length; i++)
+    for (ii = 0; ii < befList.length; ii++)
+        if (befList[i] < befList[ii]) befList[i]=[befList[ii],befList[ii]=befList[i]][0];
+befList;
+*/
