fbf292b6ca39e0bea8887526437a0042342c7038
kuroziten <33644935+kuroziten@users.noreply.github.com>
2023-03-08 18:17:23 +0900

    デュラチャにトリップつけてログインするやつ！

--- drrr_trip.py
+++ drrr_trip.py
@@ -2,7 +2,7 @@ import requests
 import urllib.parse
 user_icon = "bear"
 user_name = "駆け抜ける熊(偽物)"
-room_id = "010710098f7361be624f8b269dd1cf84"
+room_id = "3762bf77fb8249d3d9b69f6bcda036d4"
 session = requests.session()
 session.cookies.set("profile", urllib.parse.quote(user_name) + ":" + user_icon + ":ja-JP:ziten")
 req = session.get("https://drrrkari.com")
