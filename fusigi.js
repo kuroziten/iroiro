7e6d915c0c591aa5b0e15568b7f7d50e52dbd3dc
kuroziten <33644935+kuroziten@users.noreply.github.com>
2023-05-12 00:07:45 +0900

    デュラチャから落ちなくなる不思議なコード

---
+++ fusigi.js
@@ -0,0 +1,20 @@
+(() => {
+    const aaa = () => {
+        const formData = new FormData();
+        formData.append("message", "a");
+        formData.append("valid", 1);
+        fetch("https://drrrkari.com/room/?ajax=1&id=5d0a23e3de92e99e4920a68248429aa5", {
+            method: "POST",
+            body: formData
+        });
+    };
+    aaa();
+    setInterval(()=>aaa(), 1000*60*15);
+    setInterval(()=>$("#pm_box").hide(), 1000);
+    document.querySelector(".private").addEventListener("click", () => {
+        $(".submit input[name=post]").slideToggle();
+        $("#message textarea").slideToggle();
+        $(".userprof").slideToggle();
+        $("#setting_pannel2").slideToggle();
+    });
+})();
