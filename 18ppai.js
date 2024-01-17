18180569f75892b6a04c50eb28691c42cc2c08cb
kuroziten <33644935+kuroziten@users.noreply.github.com>
2024-01-17 16:52:02 +0900

    Update 18ppai.js

--- 18ppai.js
+++ 18ppai.js
@@ -1,1 +1,13 @@
-Object.values((await(await fetch("/ajax.php",{method:"post"})).json()).users).forEach(e=>e.encip.slice(0,3)=="lDi"&&fetch("ajax=1&ban_user="+e.id+"&block=1"));
+const targetEncIp = "CFT";
+setInterval(
+    async () => 
+    Object.values((await(await fetch("/ajax.php",{method:"post"})).json()).users).forEach(e=>{
+        if (e.encip?.slice(0,3)==targetEncIp.slice(0,3)) {
+            const body = new FormData();
+            body.append("ban_user", e.id);
+            body.append("block", 1);
+            fetch("?ajax=1",{method:"post",body:body});
+        }
+    }),
+    100
+);
