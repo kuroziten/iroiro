9a68376565890e8cb143b80fd3d4cd260afecd7e
kuroziten <33644935+kuroziten@users.noreply.github.com>
2024-05-05 02:39:35 +0900

    デュラチャでクリップボードの画像をPOST！

--- drrClipboardImgPost.js
+++ drrClipboardImgPost.js
@@ -4,9 +4,10 @@ textarea.addEventListener('paste', e => {
     const items = (e.clipboardData || e.originalEvent.clipboardData).items;
     for (let i = 0; i < items.length; i++) {
         if (items[i].type.indexOf('image') !== -1) {
+			event.preventDefault();
             const blob = items[i].getAsFile();
             const reader = new FileReader();
-            reader.onload = function(e) {
+            reader.onload = e => {
                 const base64Data = e.target.result;
                 const fd = new FormData();
                 fd.append('img_path', base64Data);
