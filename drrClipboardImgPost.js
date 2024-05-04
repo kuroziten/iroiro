a7d7e9ae2668cd0300a1cfb617f4faebd999907c
kuroziten <33644935+kuroziten@users.noreply.github.com>
2024-05-05 03:49:26 +0900

    デュラチャでクリップボードの画像をPOST！

--- drrClipboardImgPost.js
+++ drrClipboardImgPost.js
@@ -1,23 +1,14 @@
-const textarea = document.querySelector('textarea');
-
-textarea.addEventListener('paste', e => {
-    const items = (e.clipboardData || e.originalEvent.clipboardData).items;
-    for (let i = 0; i < items.length; i++) {
-        if (items[i].type.indexOf('image') !== -1) {
+document.querySelector('textarea').addEventListener('paste', e => {
+    for (item of (e.clipboardData || e.originalEvent.clipboardData).items) {
+        if (item.type.indexOf('image') !== -1) {
 			event.preventDefault();
-            const blob = items[i].getAsFile();
-            const reader = new FileReader();
-            reader.onload = e => {
-                const base64Data = e.target.result;
-                const fd = new FormData();
-                fd.append('img_path', base64Data);
-                fd.append('upimg', 'アップロード');
-                fetch('/room/', {
-                    method: 'POST',
-                    body: fd
-                })
-            };
-            reader.readAsDataURL(blob);
+            const fd = new FormData();
+            fd.append('img_path', item.getAsFile());
+            fd.append('upimg', 'アップロード');
+            fetch('/room/', {
+                method: 'POST',
+                body: fd
+            });
         }
     }
 });
