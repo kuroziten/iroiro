3c7d74577bd8885c15ebd67fd0ca7037d43cd1a2
kuroziten <33644935+kuroziten@users.noreply.github.com>
2025-01-24 16:52:53 +0900

    時間計算！

--- jikankeisan.js
+++ jikankeisan.js
@@ -1,33 +1,39 @@
+function timeCalc(time, reduceTime) {
 
-function timeCalc(time, reqTime) {
+    const timeS = time.split(":").map(e => Number(e));
+    const timeMinute = timeS[0] * 60 + timeS[1];
 
-	const ls = time.split(":").map(e => Number(e));
-	const reqLs = reqTime.split(":").map(e => Number(e));;
-	let timeNum = (ls[0] * 60 + ls[1] - (60 * reqLs[0] + reqLs[1])) / 60;
-	if (timeNum < 0) {
-		timeNum+=24;
-	}
-	const res = String(Math.floor(timeNum)).padStart(2, "0") + ":" + String(timeNum % 1 * 60).padStart(2, "0");
-	return res;
+    const reduceTimeS = reduceTime.split(":").map(e => Number(e));
+    const reduceMinute = reduceTimeS[0] * 60 + reduceTimeS[1];
+
+    let resTime = timeMinute - reduceMinute;
+    if (resTime < 0) {
+        resTime += 60 * 24;
+    }
+    const hh = String(Math.floor(resTime / 60)).padStart(2, "0");
+    const mm = String(resTime % 60).padStart(2, "0");
+
+    return hh + ":" + mm;
 }
 
 function timeCalcAction(time, list) {
-	let res = time;
-	const resList = [];
-	for (const e of list.reverse()) {
-		res = timeCalc(res, e[1]);
-		resList.push([e[0], res]);
-	}
-	for (const e of resList.reverse()) {
-		console.log(e[0], e[1]);
-	}
+    let res = time;
+    const resList = [];
+    for (const e of list.reverse()) {
+
+        res = timeCalc(res, e[1]);
+        resList.push([e[0], res]);
+    }
+    for (const e of resList.reverse()) {
+        console.log(e[0], e[1]);
+    }
 };
 
-timeCalcAction("22:00", [
-	["ご飯の準備", "03:00"],
-	["ご飯食べる", "05:00"],
-	["寝る", "08:00"],
-	["起きて準備", "01:00"],
-	["家を出る", "02:00"],
-	["仕事開始", "08:30"],
+timeCalcAction("21:00", [
+    ["ご飯の準備", "03:00"],
+    ["ご飯食べる", "05:00"],
+    ["寝る", "08:00"],
+    ["起きて準備", "01:00"],
+    ["家を出る", "01:40"],
+    ["仕事開始", "08:30"],
 ]);
