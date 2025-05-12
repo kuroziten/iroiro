a9959454cb8b8ce8fcb4e39ad7011596812f0fbe
kuroziten <33644935+kuroziten@users.noreply.github.com>
2025-05-13 00:00:23 +0900

    キックしたりバンしたり！

--- drrKickBan.js
+++ drrKickBan.js
@@ -1,33 +1,35 @@
 // banする対象の名前
 banTargetName = [
-  "スカイ1"
+"スカイ1"
 ];
 // banする対象のencip
 banTargetEncip = [
-  "aaaaa"
+"aaaaa"
 ];
 // kickする対象の名前
 kickTargetName = [
-  "スカイ"
+"スカイ"
 ];
 // kickする対象のencip
 kickTargetEncip = [
-  "CFT2i"
+"CFT2i"
 ];
 
+new MutationObserver(async e=>{
 json = await (await fetch("/ajax.php", {method:"post"})).json();
 users = Object.values(json.users).map(e => ({name: e.name, encip: e.encip?.split("").slice(0, 5), id: e.id}));
 
 for (u of users) {
-  console.log(u.name);
-  if (banTargetName.some(e => e === u.name) || banTargetEncip.some(e => e === u.encip)) {
+if (banTargetName.some(e => e === u.name) || banTargetEncip.some(e => e === u.encip)) {
     const formData = new FormData();
     formData.append("ban_user", u.id);
     formData.append("block", 1);
     fetch("/room/?ajax=1", {method: "post", body: formData});
-  } else if (kickTargetName.some(e => e === u.name) || kickTargetEncip.some(e => e === u.encip)) {
+} else if (kickTargetName.some(e => e === u.name) || kickTargetEncip.some(e => e === u.encip)) {
     const formData = new FormData();
     formData.append("ban_user", u.id);
     fetch("/room/?ajax=1", {method: "post", body: formData});
-  }
 }
+}
+    
+}).observe(talks,{childList:true});
