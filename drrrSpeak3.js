b276519b46faeda26f255695f62f703dd87b9ec4
kuroziten <33644935+kuroziten@users.noreply.github.com>
2025-06-17 22:07:45 +0900

    Create drrrSpeak3.js

---
+++ drrrSpeak3.js
@@ -0,0 +1,28 @@
+value = document.querySelector("input").value;
+function waitForAudioToEnd(audio) {
+  return new Promise((resolve) => {
+    audio.addEventListener('ended', resolve, { once: true });
+  });
+}
+
+async function p() {
+	const inputList = document.querySelectorAll("input");
+
+	if (inputList[0].value !== value) {
+		for (i = 1; i < inputList.length; i++) {
+			const e = inputList[i];
+			if (e.value === value) {
+				const e = inputList[i - 1];
+				audio = new Audio(await(await(await fetch("http://localhost:3001?msg='" + e.parentNode.querySelector("message").textContent + "'")).text()));
+				audio.play();
+				await audio.play(); // 再生を開始
+				await waitForAudioToEnd(audio); // 再生終了まで待機
+				value = inputList[i - 1].value;
+				break;
+			}
+		}
+	}
+	await new Promise(r => setTimeout(r, 2000));
+	p();
+}
+p();
