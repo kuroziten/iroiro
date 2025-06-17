114933557ab49b0c9bdaa3772b66b6d61adf4a3e
kuroziten <33644935+kuroziten@users.noreply.github.com>
2025-06-17 22:43:01 +0900

    Update drrrSpeak3.js

--- drrrSpeak3.js
+++ drrrSpeak3.js
@@ -1,11 +1,12 @@
 value = document.querySelector("input").value;
 function waitForAudioToEnd(audio) {
-  return new Promise((resolve) => {
-    audio.addEventListener('ended', resolve, { once: true });
-  });
+	return new Promise((resolve) => {
+		audio.addEventListener('ended', resolve, { once: true });
+	});
 }
 
 async function p() {
+	console.log("value", value);
 	const inputList = document.querySelectorAll("input");
 
 	if (inputList[0].value !== value) {
@@ -13,16 +14,19 @@ async function p() {
 			const e = inputList[i];
 			if (e.value === value) {
 				const e = inputList[i - 1];
-				audio = new Audio(await(await(await fetch("http://localhost:3001?msg='" + e.parentNode.querySelector("message").textContent + "'")).text()));
-				audio.play();
-				await audio.play(); // 再生を開始
-				await waitForAudioToEnd(audio); // 再生終了まで待機
+				const msg = e.parentNode.querySelector("message").textContent;
+				if (msg !== "") {
+					audio = new Audio(await(await(await fetch("http://localhost:3001?msg='" + e.parentNode.querySelector("message").textContent + "'")).text()));
+					audio.play();
+					await audio.play(); // 再生を開始
+					await waitForAudioToEnd(audio); // 再生終了まで待機					
+				}
 				value = inputList[i - 1].value;
 				break;
 			}
 		}
 	}
-	await new Promise(r => setTimeout(r, 2000));
+	await new Promise(r => setTimeout(r, 1000));
 	p();
 }
 p();
