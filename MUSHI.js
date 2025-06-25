0988dbe0beb2a6c0e0000d8c712d5d58f82af699
kuroziten <33644935+kuroziten@users.noreply.github.com>
2025-06-25 14:35:50 +0900

    無視するやつ！

--- MUSHI.js
+++ MUSHI.js
@@ -1,4 +1,7 @@
-target = /ヌイカ/;
-rem = () => talks.querySelectorAll("dt").forEach(e=>(target.test(e.textContent))&&e.parentNode.remove());
-new MutationObserver(e=>rem()).observe(talks,{childList:true});
-rem();
+str = prompt();
+if (str !== "") {
+	target = new RegExp(str);
+  rem = () => talks.querySelectorAll("dt").forEach(e=>(target.test(e.textContent))&&e.parentNode.remove());
+  new MutationObserver(e=>rem()).observe(talks,{childList:true});
+  rem();
+}
