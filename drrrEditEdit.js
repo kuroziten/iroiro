86028a5f5084a34ac72a218369e00110e89b4881
kuroziten <33644935+kuroziten@users.noreply.github.com>
2025-07-09 09:36:52 +0900

    デュラチャ画面を改造するやつ！

--- drrrEditEdit.js
+++ drrrEditEdit.js
@@ -57,7 +57,18 @@ overflow: scroll;
 
 [encip] [no] {
 display: flex;
+}
+
+#inUser [encip]:nth-child(even) {
 background-color: pink;
+}
+
+#inUser [encip]:nth-child(odd) {
+background-color: cadetblue;
+}
+
+#outUser [encip] {
+background-color: gray;
 }
 
 [encip] [no] {
@@ -86,18 +97,35 @@ padding-left: 5px;
     document.body.append(style);
 
     // 追加要素作成
-    const newMenu = await new Promise(r => {
-        const menu = document.createElement("div");
-        menu.id = "newMenu";
-        talks_box.append(menu);
-        r(menu);
+    await new Promise(r => {
+        const newMenu = document.createElement("div");
+        newMenu.id = "newMenu";
+        talks_box.append(newMenu);
+
+        const inUser = document.createElement("div");
+        inUser.id = "inUser";
+        const outUser = document.createElement("div");
+        outUser.id = "outUser";
+        newMenu.append(inUser);
+        newMenu.append(outUser);
+        r();
     });
+
+    const newMenu = document.querySelector("#newMenu");
+    const inUser = document.querySelector("#inUser");
+    const outUser = document.querySelector("#outUser");
 
     // DBの定義情報
     const dbName = "20250627";
     const ENCIPT = "ENCIPT";
     const USERT = "USERT";
     const IMGT = "IMGT";
+
+    // その他の固定値
+    const maxRangBound = encip => IDBKeyRange.bound(
+        [encip, new Date(0)],               // 1970年1月1日
+        [encip, new Date(8640000000000000)] // JSで扱える最大日付
+    );
 
     // DB削除
     // indexedDB.deleteDatabase(dbName);
@@ -110,8 +138,11 @@ padding-left: 5px;
         const db = event.target.result;
 
         // ENCIPテーブルのストア作成
+        // ENCIP | UPDT_DATE | COUNT
+        // ENCIP | 更新日時  | ユーザ数
         const ENCIPTS = db.createObjectStore(ENCIPT, { keyPath: "ENCIP" });
         ENCIPTS.createIndex("ENCIPI", "UPDT_DATE", { unique: false });
+        ENCIPTS.createIndex("ENCIPI_COUNT", "COUNT", { unique: false });
 
         // USERテーブルのストア作成
         const USERTS = db.createObjectStore(USERT, {keyPath: "NO", autoIncrement: true});
@@ -119,6 +150,7 @@ padding-left: 5px;
         USERTS.createIndex("USERI2", ["ENCIP", "NAME"], { unique: true });
 
         // IMGテーブルのストア作成
+        // URL | HASH | BLOB |
         const IMGTS = db.createObjectStore(IMGT, { keyPath: "ID"});
         IMGTS.createIndex("IMGI", "INST_DATE", { unique: false });
     };
@@ -126,60 +158,81 @@ padding-left: 5px;
     // DB取得
     const db = await new Promise(r => request.onsuccess = e => r(e.target.result));
 
+    // talkの最後のID（画像の処理は連続してする必要がないので）
+    let lastTalkId = null;
+
+    // ○○さんが退室しましたの最後のIDを取得
+    let lastExitId = null;
+    let res = -1;
+
     const act = async () => {
 
-        // デュラチャ情報取得
-        const res = await(await fetch("/ajax.php", {method: "POST"})).json();
+        // 入退室が無い場合はPOSTしないで使い回す
+        for (const talk of document.querySelectorAll(".talk")) {
+            if (talk.classList.contains("system") || talk.querySelector("a")) {
+                if (lastExitId !== talk.id) {
+                    lastExitId = talk.id;
+                    res = await(await fetch("/ajax.php", {method: "POST"})).json();
+                }
+                break;
+            }
+        }
+
+        // ENCIPとNAMEの組み合わせチェック
+        const updatedTargetEncipUser = new Map();
+
+        // 画像テーブル更新フラグ
+        let imgtUpdtFlg = true;
+        (talk => {
+            if (talk.id !== lastTalkId) {
+                // taklIdが最後に確認したものと異なる場合
+                // lastTalkIdを更新する
+                lastTalkId = talk.id;
+            } else {
+                // それ以外の場合
+                // 画像テーブル更新フラグをfalseにする
+                imgtUpdtFlg = false;
+            }
+        })(Object.values(res.talks).slice(-1)[0]);
 
         const now = new Date();
 
-        // ENCIPT更新
-        await new Promise(async r => {
+        // 入室中のencip
+        const inEncip = new Set();
 
-            const transaction = db.transaction(ENCIPT, "readwrite");
-            const store = transaction.objectStore(ENCIPT);
-
-            for (const user of Object.values(res.users)) {
-                const encip = user.encip.substring(0, 5);
-                store.put({
-                    ENCIP: encip,
-                    UPDT_DATE: now
-                });
-            }
-
-            transaction.oncomplete = () => r();
-        });
+        const updtEncipList = new Set();
 
         // USERT更新
         await new Promise(async r => {
 
+            // ストアを取得
             const transaction = db.transaction(USERT, "readwrite");
             const store = transaction.objectStore(USERT);
 
+            // usersとtalksからユーザ情報を取得する
             const users = [];
             for (const user of Object.values(res.users)) {
+                // usersをループして取得
+                const encip = user.encip.substring(0, 5);
                 users.push({
-                    encip: user.encip.substring(0, 5),
+                    encip: encip,
                     name: user.name
                 });
-            }
-            for (const talk of Object.values(res.talks)) {
-                if (talk.type === "enter") {
-                    const encip = talk.encip.substring(0, 5);
-                    const name = talk.name;
-                    if (!users.some(e => e.encip === encip && e.name === name)) {
-                        users.push({
-                            encip: talk.encip.substring(0, 5),
-                            name: talk.name
-                        });
-                    }
-                }
-            }
+            };
 
             for (const user of users) {
 
                 const encip = user.encip.substring(0, 5);
                 const name = user.name;
+
+                if (!updatedTargetEncipUser.get(encip)) {
+                    updatedTargetEncipUser.set(encip, new Set());
+                    updatedTargetEncipUser.get(encip).add(name);
+                    inEncip.add(encip);
+                    updtEncipList.add(encip);
+                } else if (!updatedTargetEncipUser.get(encip).has(name)) {
+                    updatedTargetEncipUser.get(encip).add(name);
+                }
 
                 const index = store.index("USERI2");
 
@@ -195,45 +248,132 @@ padding-left: 5px;
                             "NAME": name,
                             "UPDT_DATE": now,
                             "INST_DATE": now,
-                        }).onsuccess = () => r()
+                        }).onsuccess = () => r();
                     });
                 }
             }
 
+            for (const talk of Object.values(res.talks)) {
+                // talksをループ
+                if (talk.type === "enter") {
+                    const encip = talk.encip.substring(0, 5);
+                    const name = talk.name;
+                    // 未取得の場合は処理
+                    if (!users.some(e => e.encip === encip && e.name === name)) {
+                        // USERTに存在するか確認
+                        const index = store.index("USERI2");
+                        const result = await new Promise(r => index.get([encip, name]).onsuccess = event => r(event.target.result));
+                        if (!result) {
+                            // 存在しない場合は登録する
+                            await new Promise(r => {
+                                store.add({
+                                    "ENCIP": encip,
+                                    "NAME": name,
+                                    "UPDT_DATE": now,
+                                    "INST_DATE": now,
+                                }).onsuccess = () => r();
+                            });
+                            updtEncipList.add(encip);
+                        }
+                    };
+                };
+            };
+
             transaction.oncomplete = () => r();
         });
 
-        // IMGT更新
+        // ENCIPT更新
         await new Promise(async r => {
-            for (const talk of Object.values(res.talks)) {
-                const image = talk.image;
-                if (image) {
-                    const transaction = db.transaction(IMGT, "readonly");
-                    const store = transaction.objectStore(IMGT);
-                    const id = talk.id;
-                    const result = await new Promise(r => store.get(id).onsuccess = event => r(event.target.result));
-                    if (!result) {
-                        const blob = await(await fetch("https://drrrkari.com/" + talk.image)).blob();
-                        const transaction = db.transaction(IMGT, "readwrite");
-                        const store = transaction.objectStore(IMGT);
-                        await new Promise(r => {
-                            store.add({
-                                ID: id,
-                                BLOB: blob,
-                                INST_DATE: new Date()
-                            }).onsuccess = () => r();
-                        });
-                    }
-                }
+
+            const alreadySetEncip = new Set();
+            for (const encip of updtEncipList) {
+
+                // encipが重複していたら（同じ人が二人入室していたら）2つ目からは処理をスキップ
+                if (alreadySetEncip.has(encip)) continue;
+                alreadySetEncip.add(encip);
+
+                // encipに紐づくUSERTのレコード数を確認
+                const count = await new Promise(async r => {
+                    const transaction = db.transaction(USERT, "readonly");
+                    const store = transaction.objectStore(USERT);
+
+                    const index = store.index("USERI1");
+
+                    const range = maxRangBound(encip);
+
+                    const request = index.count(range);
+
+                    request.onsuccess = () => r(request.result);
+                });
+
+                const transaction = db.transaction(ENCIPT, "readwrite");
+                const store = transaction.objectStore(ENCIPT);
+                store.put({
+                    ENCIP: encip,
+                    UPDT_DATE: now,
+                    COUNT: count
+                });
             }
+
             r();
         });
+
+        // IMGT更新
+        if (imgtUpdtFlg) {
+            await new Promise(async r => {
+                for (const talk of Object.values(res.talks)) {
+                    // talksをループ
+
+                    // 画像URLを取得
+                    const image = talk.image;
+
+                    if (image) {
+                        // 画像URLの場合
+
+                        // 画像IDを取得
+                        const id = talk.id;
+
+                        // トランザクション開始
+                        const transaction = db.transaction(IMGT, "readonly");
+                        const store = transaction.objectStore(IMGT);
+
+                        // 画像IDからレコード数を取得
+                        const result = await new Promise(r => store.count(id).onsuccess = event => r(event.target.result));
+
+                        if (result === 0) {
+                            // 取得したレコード数が0の場合
+
+                            // 画像のblobを取得
+                            const blob = await(await fetch("https://drrrkari.com/" + talk.image)).blob();
+
+                            // トランザクションを開始
+                            const transaction = db.transaction(IMGT, "readwrite");
+                            const store = transaction.objectStore(IMGT);
+
+                            // データを追加
+                            await new Promise(r => {
+                                store.add({
+                                    ID: id,
+                                    BLOB: blob,
+                                    INST_DATE: new Date()
+                                }).onsuccess = () => r();
+                            });
+                        } else {
+                        }
+                    }
+                }
+                r();
+            });
+        } else {
+        };
 
         // 0: ユーザ, 1: 画像
         let mode = 0;
         // 画面更新テスト
         await new Promise(r => {
             if (mode  === 0) {
+
+                // ENCIPTからENCIP一覧を取得する
                 const transaction = db.transaction(ENCIPT, "readonly");
                 const store = transaction.objectStore(ENCIPT);
                 const index = store.index("ENCIPI");
@@ -241,34 +381,77 @@ padding-left: 5px;
                 const twelveHoursAgo = new Date();
                 twelveHoursAgo.setHours(twelveHoursAgo.getHours() - 12);
 
-                // 範囲指定
+                // 範囲指定（12時間前から現在まで）
                 const range = IDBKeyRange.bound(twelveHoursAgo, new Date());
 
-                // 取得
-                const request = index.openCursor(range, "prev");
+                // 結果を取得
+                const request = index.openCursor(range, "next");
+
+                let encipEIndex = 0;
+
+                let inUserIndex = 0;
+                let outUserIndex = 0;
 
                 request.onsuccess = function(event) {
                     const cursor = event.target.result;
                     if (cursor) {
-                        const encip = event.target.result.value.ENCIP;
-                        let encipE = newMenu.querySelector(`[encip="${encip}"]`);
-                        if (!encipE) {
-                            encipE = document.createElement("div");
-                            encipE.setAttribute("encip", encip);
+                        // encipを取得
+                        const encip = cursor.value.ENCIP;
+                        console.log("encip", encip);
+                        // encipのHTML要素を取得
+                        let encipE;
+                        if (inEncip.has(encip)) {
+                            encipE = newMenu.querySelector(`[encip="${encip}"]`);
+                            if (!encipE) {
+                                // 存在しない場合は作成して追加する
+                                encipE = document.createElement("div");
+                                encipE.setAttribute("encip", encip);
+                                inUser.append(encipE);
+                            }
+                            // 順番が変わった場合は並び替える
+                            if (inUser.querySelectorAll("[encip]")[inUserIndex] !== encipE) {
+                                if (inUserIndex === 0) {
+                                    inUser.prepend(encipE);
+                                } else if (inUser.querySelectorAll("[encip]")[inUserIndex] === undefined) {
+                                    inUser.append(encipE);
+                                } else {
+                                    inUser.querySelectorAll("[encip]")[inUserIndex].before(encipE);
+                                }
+                            }
+                            inUserIndex++;
+                        } else {
+                            encipE = newMenu.querySelector(`[encip="${encip}"]`);
+                            if (!encipE) {
+                                // 存在しない場合は作成して追加する
+                                encipE = document.createElement("div");
+                                encipE.setAttribute("encip", encip);
+                                outUser.append(encipE);
+                            }
+                            // 順番が変わった場合は並び替える
+                            if (outUser.querySelectorAll("[encip]")[outUserIndex] !== encipE) {
+                                if (outUserIndex === 0) {
+                                    outUser.prepend(encipE);
+                                } else if (outUser.querySelectorAll("[encip]")[outUserIndex] === undefined) {
+                                    outUser.append(encipE);
+                                } else {
+                                    outUser.querySelectorAll("[encip]")[outUserIndex].before(encipE);
+                                }
+                            }
+                            outUserIndex++;
                         }
-                        newMenu.prepend(encipE);
 
+                        // ユーザーが居るところを指定
                         const transaction = db.transaction(USERT, "readonly");
                         const store = transaction.objectStore(USERT);
 
+                        // Aさんを名指しで呼ぶ方法を指定
                         const index = store.index("USERI1");
+                        const range = maxRangBound(encip);
 
-                        const range = IDBKeyRange.bound(
-                            [encip, new Date(0)],                   // 1970年1月1日
-                            [encip, new Date(8640000000000000)]     // JSで扱える最大日付
-                        );
+                        // Aさんを呼ぶ
+                        const res = index.openCursor(range, 'next');
 
-                        const res = index.openCursor(range, 'prev');
+                        let i = 0;
                         res.onsuccess = event => {
                             const cursor = event.target.result;
                             if (cursor) {
@@ -288,8 +471,18 @@ padding-left: 5px;
                                     userE.append(userEC1);
                                     userE.append(userEC2);
                                 }
-                                userE.querySelector("*").textContent = formatDateWithWeekday(e.UPDT_DATE);
-                                encipE.prepend(userE);
+                                userE.querySelector("div").textContent = formatDateWithWeekday(e.UPDT_DATE);
+                                (e => {
+                                    if (e !== userE) {
+                                        if (e === undefined) {
+                                            encipE.append(userE);
+                                        } else {
+                                            e.before(userE);
+                                        }
+                                    }
+                                })(encipE.querySelectorAll("[no]")[i]);
+                                i++;
+                                console.log(i, e.NAME, e.ENCIP);
                                 cursor.continue();
                             }
                         };
