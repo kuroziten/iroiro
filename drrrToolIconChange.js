82f4bdc92ca661f0950bc3a8ffe6b980f0da9720
kuroziten <33644935+kuroziten@users.noreply.github.com>
2025-08-07 10:59:57 +0900

    Update drrrToolIconChange.js

--- drrrToolIconChange.js
+++ drrrToolIconChange.js
@@ -1,89 +1,82 @@
-// <button popovertarget="my-popover">ポップオーバーを開く</button>
-// <div popover id="my-popover">私から皆さんへ、こんにちは！</div>
+const style = document.createElement("style");
+style.textContent = `
+  #popover1:popover-open {
+		width: 50%;
+    height: 50%;
+    background-color: rgba(0,0,0,1);
+    display: block;
+    margin: auto auto;
+  }
+`;
+document.body.append(style);
 
-const button = document.createElement("button");
-button.textContent = "ちんぽ";
-button.setAttribute("popovertarget", "my-popover");
-document.body.prepend(button);
+// DB作成 & 取得
+const iconChange20250806Db = await new Promise(resolve => {
+  const request = indexedDB.open("iconChange20250806", 1);
+  request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    const store = db.createObjectStore("iconChange20250806", { keyPath: "name" });
+  };
+  request.onsuccess = () => {
+    resolve(event.target.result);
+  };
+});
 
-const popover = document.createElement("div");
-popover.setAttribute("popover", "");
-popover.id = "my-popover";
-popover.textContent = "ちんこ";
-document.body.append(popover);
+// DBに追加する関数
+const iconChange20250806DbAdd = async (name, data) => await new Promise(resolve => {
+  const transaction = iconChange20250806Db.transaction(["iconChange20250806"], "readwrite");
+  const store = transaction.objectStore("iconChange20250806");
+  const result = store.put({
+    name : name,
+    data: data
+  });
+  result.onsuccess = () => {
+    console.log("成功だにょ");
+    resolve();
+  }
+});
 
-popover.showPopover();
+const getImgData = async url => {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('Fetch failed');
 
+  const arrayBuffer = await response.arrayBuffer();
 
-// // DB作成 & 取得
-// const iconChange20250806Db = await new Promise(resolve => {
-//   const request = indexedDB.open("iconChange20250806", 1);
-//   request.onupgradeneeded = function(event) {
-//     const db = event.target.result;
-//     const store = db.createObjectStore("iconChange20250806", { keyPath: "name" });
-//   };
-//   request.onsuccess = () => {
-//     resolve(event.target.result);
-//   };
-// });
+	return new Blob([arrayBuffer]);
+};
 
-// // DBに追加する関数
-// const iconChange20250806DbAdd = async (name, data) => await new Promise(resolve => {
-//   const transaction = iconChange20250806Db.transaction(["iconChange20250806"], "readwrite");
-//   const store = transaction.objectStore("iconChange20250806");
-//   const result = store.put({
-//     name : name,
-//     data: data
-//   });
-//   result.onsuccess = () => {
-//     console.log("成功だにょ");
-//     resolve();
-//   }
-// });
-
-// const div = document.createElement("div");
-// div.setAttribute("popover", "auto");
-// div.id = "popover1";
-
-// const view = document.createElement("div");
-// view.textContent = "ちんぽ";
-// div.append(view);
-
-// document.body.append(div);
-
-// const style = document.createElement("style");
-// style.textContent = `
-// 	#popover1 {
-//   	display: none;
-//   }
-//   #popover1:popover-open {
-// 		width: 100%;
-//     height: 100%;
-//     background-color: rgba(0,0,0,.1);
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;    
-//   }
-// `;
-// // document.body.append(style);
-
-// const li = document.createElement("li");
-// document.querySelector(".menu").prepend(li);
-
-// const u = document.createElement("u");
-// u.textContent = "アイコン設定";
-// li.append(u);
-
-// li.addEventListener("click", () => {
-//   console.log("agi", div);
-//   try {
-// 	  div.showPopver();
-//   } catch(e) {
-//     console.log(e);
-//   }
-//   console.log("agi1");
-// });
+const bdt = await getImgData("https://pbs.twimg.com/media/FgUYH2WVQAA9IRs?format=jpg");
+await iconChange20250806DbAdd("ヌイカ2", bdt);
+document.querySelector("dt").style.background = "transparent url('" + URL.createObjectURL(bdt) + "') no-repeat center top";
 
 
-// // div.showPopover();
-// // div.hidePopover();
+const div = document.createElement("div");
+div.setAttribute("popover", "");
+div.id = "popover1";
+
+const view = document.createElement("div");
+view.textContent = "ちんぽ";
+div.append(view);
+
+document.body.append(div);
+
+const li = document.createElement("li");
+document.querySelector(".menu").prepend(li);
+
+const u = document.createElement("u");
+u.textContent = "アイコン設定";
+li.append(u);
+
+li.addEventListener("click", () => {
+  console.log("agi", div);
+  try {
+	  div.showPopover();
+  } catch(e) {
+    console.log(e);
+  }
+  console.log("agi1");
+});
+
+
+// div.showPopover();
+// div.hidePopover();
